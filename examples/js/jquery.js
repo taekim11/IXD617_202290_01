6b6adab32617aaee8310a74c7a6024e0d96aeeaa
taekim@tae-kim-design.com <taekimdesigns@gmail.com>
2022-09-19 15:57:10 -0700

    mofied css, jquery, and js

--- examples/js/jquery.js
+++ examples/js/jquery.js
@@ -1,14 +1,30 @@
 
-$(() => {
+$(()=>{
 
+    // ACCORDION
     $(".accordion dt").on("click", function(e) {
-        //$(this).next().show();
-        //$(this).next().toggle();
-        //$(this).next().slidedown();
-        //$(this).next().slideToggle();
+        // $(this).next().show();
+
+        // $(this).next().toggle();
+
+        // $(this).next().slideDown();
+
+        // $(this).next().slideToggle();
 
         $(this).next().slideDown()
             .siblings("dd").slideUp();
     });
+
+    /* TAB GROUP */
+    $(".tabgroup .tab").on("click", function(e) {
+        let id = $(this).index();
+
+        $(this).addClass("active")
+            .siblings().removeClass("active")
+
+        $(this).closest(".tabgroup")
+            .find(".content").eq(id).addClass("active")
+            .siblings().removeClass("active")
+    })
 
 });

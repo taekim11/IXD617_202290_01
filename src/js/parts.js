419a89308518110e7d2af6ac808b45bbf41f20d0
taekim@tae-kim-design.com <taekimdesigns@gmail.com>
2022-11-13 20:01:14 -0800

    updated pages

--- src/js/parts.js
+++ src/js/parts.js
@@ -2,7 +2,7 @@ import { templater } from "./functions.js";
 
 
 export const makeAnimalList = templater(({id,name,type,breed,img})=>`
-<a class="animallist-item" href="#animal-profile-page" data-id="${id}">
+<a class="animallist-item animal-jump" href="#animal-profile-page" data-id="${id}">
     <div class="animallist-image"><img src="${img}"></div>
     <div class="animallist-body">
         <div class="animallist-name">${name}</div>
@@ -11,3 +11,21 @@ export const makeAnimalList = templater(({id,name,type,breed,img})=>`
     </div>
 </a>
 `)
+
+export const makeUserProfilePage = ({name,email,username,img})=>`
+<div>
+    <div class="user-profile-image"><img src="${img}"></div>
+    <div class="user-profile-body">
+        <div class="user-profile-name">${name}</div>
+        <div class="user-profile-breed">@${username}</div>
+        <div class="user-profile-email">${email}</div>
+    </div>
+    <a href="#" class="js-logout">Log out</a>
+</div>
+`
+
+export const makeAnimalProfileDescription = ({name,type,breed})=>`
+<h2>${name}</h2>
+<div>${type}</div>
+<div>${breed}</div>
+`
